a2bc2aeb62c9d5ba50471444a7a3ee4dc2bca52a
etoup <etoupcom@126.com>
2016-04-13 16:55:35 +0800

    new

--- app/Repositories/Open/My/EloquentMyRepository.php
+++ app/Repositories/Open/My/EloquentMyRepository.php
@@ -11,7 +11,7 @@ use App\Models\Loop\Loops;
 class EloquentMyRepository implements MyRepositoryContract
 {
 
-    public function loops($uid,$page,$take = 10){
+    public function loops($uid,$page,$take = 1){
         $skip = $page * $take;
         $loops = Loops::with('pictures','users')->where('users_id',$uid)->orderBy('id')->skip($skip)->take($take)->get();
         return $loops;
