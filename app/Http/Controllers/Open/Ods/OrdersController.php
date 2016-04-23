0b27d06fcb8ee6c3afbfb5e3c49b57107381d0b0
etoup <etoupcom@126.com>
2016-04-23 23:51:51 +0800

    20160423-09

--- app/Http/Controllers/Open/Ods/OrdersController.php
+++ app/Http/Controllers/Open/Ods/OrdersController.php
@@ -85,22 +85,22 @@ class OrdersController extends Controller
             // 使用通知里的 "商户订单号" 获取订单
             $order = $this->orders->findOrdersByNum($notify->out_trade_no);
 
-            if (!$order) { // 如果订单不存在
-                return 'Order not exist.'; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
-            }
+//            if (!$order) { // 如果订单不存在
+//                return 'Order not exist.'; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
+//            }
 
             // 如果订单存在
             // 检查订单是否已经更新过支付状态
-            if ($order->status == 10) { // 假设订单字段“支付时间”不为空代表已经支付
-                return true; // 已经支付成功了就不再更新了
-            }
+//            if ($order->status == 10) { // 假设订单字段“支付时间”不为空代表已经支付
+//                return true; // 已经支付成功了就不再更新了
+//            }
 
             // 用户是否支付成功
-            if (!$successful) {
-                // 不是已经支付状态则修改为已经支付状态
-                $order->status = 10; // 更新支付状态
-            }
-
+//            if ($successful) {
+//                // 不是已经支付状态则修改为已经支付状态
+//                $order->status = 10; // 更新支付状态
+//            }
+            $order->status = 10;
             $order->save(); // 保存订单
 
             return true; // 返回处理完成
